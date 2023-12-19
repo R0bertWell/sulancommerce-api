eed37af0215ecc55df89c70bfb35eca1c811b7b2
r0bertwell <r0bertwell@hotmail.com>
2023-12-18 22:55:00 -0300

    [WebSecurityConfig] - Added security filter chain to validate enpoints, disable cors and csrf, and creation of beans to password encode an JwtFilter.

---
+++ src/main/java/src/sulancommerce/configs/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package src.sulancommerce.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import src.sulancommerce.configs.filters.JwtRequestFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    private final JwtRequestFilter requestFilter;
+
+    public WebSecurityConfig(JwtRequestFilter requestFilter) {
+        this.requestFilter = requestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                                .requestMatchers("/authentication", "/sign-up").permitAll()
+                        //.requestMatchers(HttpMethod.GET).permitAll()
+                        //.requestMatchers(HttpMethod.POST).authenticated()
+                        //.anyRequest().authenticated()
+                )
+                .authorizeHttpRequests((apiRequests) ->
+                        apiRequests.requestMatchers("/api/**").permitAll()
+                                .requestMatchers("/**").authenticated()
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
