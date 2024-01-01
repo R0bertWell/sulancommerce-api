d2693eef995752d2bed105a5af5eaeeed8ccfc2b
r0bertwell <r0bertwell@hotmail.com>
2024-01-01 18:47:18 -0300

    [SWAGGER] - Initial configuration of swagger documentation.

--- src/main/java/src/sulancommerce/controllers/CategoryController.java
+++ src/main/java/src/sulancommerce/controllers/CategoryController.java
@@ -1,36 +1,40 @@
 package src.sulancommerce.controllers;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import src.sulancommerce.models.dtos.CategoryDTO;
-import src.sulancommerce.repositories.CategoryRepository;
 import src.sulancommerce.services.CategoryService;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/v1/category")
-public class CategoryController {
+@RequestMapping("/api/v1/categories")
+public class NewCategoryController {
 
-     private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService) {
+    public NewCategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
 
     @GetMapping("list")
-    public ResponseEntity<List<CategoryDTO>> getCategoriesByFilters(@RequestParam(defaultValue = "") String filter) {
-        List<CategoryDTO> categoryDTOList = this.categoryService.getCategoriesByFilter(filter);
-        return new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
+    public ResponseEntity<Page<CategoryDTO>> getCategoriesByFilters(@RequestParam(defaultValue = "") String filter,
+                                                                    Pageable pageable) {
+        Page<CategoryDTO> categoryDTOs = this.categoryService.getCategoriesPaged(filter, pageable);
+        return new ResponseEntity<>(categoryDTOs, HttpStatus.OK);
     }
 
-    @PostMapping("save")
+    @PostMapping("category/save")
     public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) throws Exception {
         this.categoryService.saveCategory(categoryDTO);
 
         return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "category/{categoryId}/delete")
+    public ResponseEntity<HttpStatus> deleteCategory(@PathVariable Long categoryId) throws Exception {
+        this.categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
